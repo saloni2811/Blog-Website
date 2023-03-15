eee015570d59f8bec5e98a552cc608d3d8c0612b
saloni2811 <saloni28singh2001@gmail.com>
2023-03-15 23:55:45 +0530

    Some Changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs"); 
+const ejs = require("ejs");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -10,34 +10,41 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+var posts = [];
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/" ,function(req,res){
-  res.render("home" , {startingContent : homeStartingContent});
+app.get("/", function (req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+  });
 });
 
-app.get("/about" , function(req,res){
-  res.render("about" , {abContent : aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { abContent: aboutContent });
 });
 
-app.get("/contact" , function(req,res){
-  res.render("contact" , {cContent : contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { cContent: contactContent });
 });
 
-app.get("/compose" , function(req,res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose" , function(req,res){
-  
+app.post("/compose", function (req, res) {
+
   var post = {
-    title:req.body.postTitle ,
-    content : req.body.postBody 
+    title: req.body.postTitle,
+    content: req.body.postBody
   };
-  console.log(post);
+  posts.push(post);
+
+  res.redirect("/");
 
 });
 
@@ -54,6 +61,6 @@ app.post("/compose" , function(req,res){
 
 
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
